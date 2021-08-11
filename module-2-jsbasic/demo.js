43546499fc2dd844889e6dd0a73a1cfdf4888ed9
kubokid <thangnh0511@gmail.com>
2021-08-11 21:10:45 +0700

    update

---
+++ module-2-jsbasic/demo.js
@@ -0,0 +1,25 @@
+// var x ,y,z ;
+// x = +prompt("nhập giá trị x ");
+// y = +prompt("Nhập giá trị y ");
+
+// z = x + y ; 
+// document.getElementById('result').innerHTML = z ; 
+
+// hàm prompt - lấy dữ liệu từ bàn phím 
+
+
+function pt_bacNhat() {
+    var a , b ;
+    a = document.getElementById('a').value;
+    b = document.getElementById('b').value;
+    
+    if (a==0) {
+        if (b==0){
+            document.getElementById('c').innerHTML = "Vô số Nghiệm"; 
+        } else {
+            document.getElementById('c').innerHTML = "Phương trình Vô Nghiệm";
+        }
+    } else {
+        document.getElementById('c').innerHTML = "Nghiem x = " + -b/a; 
+    }
+}
