fe96c74b2b273b020ee3f41a998ff56c551b3d8a
kubokid <thangnh0511@gmail.com>
2021-08-11 23:24:16 +0700

    update phương trình bậc 2

--- module-2-jsbasic/demo.js
+++ module-2-jsbasic/demo.js
@@ -10,24 +10,50 @@
 // Phương trình bậc 1 
 
 function pt_bacNhat() {
-    var a , b ;
-    a = document.getElementById('a').value;
-    b = document.getElementById('b').value;
-    
-    if (a==0) {
-        if (b==0){
-            document.getElementById('c').innerHTML = "Vô số Nghiệm"; 
+    var a1 , b1 ;
+    a1 = document.getElementById('a1').value;
+    b1 = document.getElementById('b1').value;
+    temp = (-b1/a1)
+    if (a1==0) {
+        if (b1==0){
+            document.getElementById('c1').innerHTML = "Vô số Nghiệm"; 
         } else {
-            document.getElementById('c').innerHTML = "Phương trình Vô Nghiệm";
+            document.getElementById('c1').innerHTML = "Phương trình Vô Nghiệm";
         }
     } else {
-        document.getElementById('c').innerHTML = "Nghiem x = " (-b/a); 
+        document.getElementById('c1').innerHTML = "Nghiem x = " + temp; 
     }
+    console.log(temp);
 }
 
 
 // Phương trình bậc 2 
 
-function pt_bacHai() {
+function pt_bacHai() {1
+    var a, b, c, delta; 
+    a = document.getElementById('a').value;
+    b = document.getElementById('b').value;
+    c = document.getElementById('c').value;
 
+    delta = b*b - a*a*c ;
+
+    if (a!=0) {
+        if (delta == 0) {
+            document.getElementById('delta').innerHTML= delta;
+            document.getElementById('result').innerHTML = `x<sub>1</sub>=  x<sub>2</sub> = ` + (-b/(2*a)) ;
+        } 
+        else {
+            if (delta > 0) {
+                document.getElementById('delta').innerHTML= delta;
+                document.getElementById('result').innerHTML = "x<sub>1</sub> = " + ((- b + Math.sqrt(delta))/(2*a)) + "<br>" + "x<sub>2</sub> = " + ((- b - Math.sqrt(delta))/(2*a)) ;
+            } else {
+                document.getElementById('delta').innerHTML= delta;
+                document.getElementById('result').innerHTML = "Phương trình vô nghiệm !";
+            }
+        }
+    } else {
+        document.getElementById('delta').innerHTML= "Không có delta";
+        document.getElementById('result').innerHTML ="Nghiệm x=" + (-c/b);
+    }
 }
+
